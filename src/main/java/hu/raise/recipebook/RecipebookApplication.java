a412d08afe42a565e50dbe8dd67ef07788bbdcf5
staraise <staraise@gmail.com>
2017-12-18 19:58:37 +0100

    init commit

---
+++ src/main/java/hu/raise/recipebook/RecipebookApplication.java
@@ -0,0 +1,12 @@
+package hu.raise.recipebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipebookApplication{
+
+    public static void main(String[] args){
+        SpringApplication.run(RecipebookApplication.class, args);
+    }
+}
